02fb01cfe1e59296216121373a170a73dd180ee3
joaopaulo33 <joaopaulo33573@gmail.com>
2023-06-25 18:57:31 -0300

    Ajuste

--- src/pages/CadastroPage/script.js
+++ src/pages/CadastroPage/script.js
@@ -6,7 +6,7 @@ document.getElementById("isAluno").addEventListener("change", function (event) {
         document.getElementById("codigo_treinador").style.display = "flex";
     }
 });
-document.getElementById("botaoCadastro").addEventListener("click", function (event) {
+document.getElementById("botaoCadastro").addEventListener("click", async function (event) {
     var nome = document.getElementById("nome").value;
     var login = document.getElementById("login").value;
     var senha = document.getElementById("senha").value;
@@ -24,7 +24,7 @@ document.getElementById("botaoCadastro").addEventListener("click", function (eve
         senha: senha,
         codigo_treinador: isAluno ? codigo_treinador : null
     };
-    fetch(`http://localhost:3000/usuarios?login=${login}`, {
+    await fetch(`http://localhost:3000/usuarios?login=${login}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
