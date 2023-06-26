bfefb591ab19cfcef93a4c4869b73d38421f62ef
Matheus Amaral <matheus.amaral.1443007@sga.pucminas.br>
2023-06-25 21:56:00 -0300

    Revert "resposta do professor aparecendo na tela do aluno"
    This reverts commit 6718588349d3bb9faf2986cff4d2d3e77be26886.

--- src/pages/Retorno_Dúvidas/script.js
+++
@@ -1,5 +0,0 @@
-function salvarDadosRespTreinador() {
-    var resposta = document.getElementById("resposta").value;
-    // Armazenar valores usando localStorage
-    localStorage.setItem("resposta", resposta);
-  }
