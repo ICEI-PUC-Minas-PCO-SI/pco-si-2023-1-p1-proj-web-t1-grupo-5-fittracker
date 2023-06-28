add506f9b2f10656e51bf68b8529efcc9dbbe5a8
joaopaulo33 <joaopaulo33573@gmail.com>
2023-06-27 23:08:48 -0300

    Ajuste

--- src/pages/treino_dieta_professor/script.js
+++ src/pages/treino_dieta_professor/script.js
@@ -1,25 +1,24 @@
 // Função para salvar os dados do treino e dieta
 function salvarDados() {
-    var treino = document.getElementById("treino").value;
-    var dieta = document.getElementById("dieta").value;
-  
-    // Criar um objeto JSON com os dados
-    var data = {
-      treino: treino,
-      dieta: dieta
-    };
-  
-    // Enviar uma requisição POST para salvar os dados no JSON Server
-    $.ajax({
-      method: "POST",
-      url: "http://localhost:3000/treino_dieta",
-      data: data,
-      success: function(response) {
-        console.log("Dados salvos com sucesso!");
-      },
-      error: function(xhr, status, error) {
-        console.log("Erro ao salvar os dados:", error);
-      }
-    });
-  }
-  
+  var treino = document.getElementById("treino").value;
+  var dieta = document.getElementById("dieta").value;
+
+  // Criar um objeto JSON com os dados
+  var data = {
+    treino: treino,
+    dieta: dieta
+  };
+  console.log("aq");
+  // Enviar uma requisição POST para salvar os dados no JSON Server
+  $.ajax({
+    method: "POST",
+    url: "http://localhost:3000/treino_dieta",
+    data: data,
+    success: function (response) {
+      console.log("Dados salvos com sucesso!");
+    },
+    error: function (xhr, status, error) {
+      console.log("Erro ao salvar os dados:", error);
+    }
+  });
+}
