bbd391d90ce33605288c2fb24e07a4a10ea3ae3b
Matheus Duarte <matheus.rodrigues.1437462@sga.pucminas.br>
2023-06-28 14:52:58 -0300

    Visual e funcionalidades tela feedback_semanal

--- src/pages/feedback_semanal/script.js
+++ src/pages/feedback_semanal/script.js
@@ -2,6 +2,24 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 const nome = urlParams.get("nome");
 
+$.ajax({
+    method: "GET",
+    url: `http://localhost:3000/usuarios`,
+    success: function (data) {
+      data.forEach(value => {
+        if (value.id == id) {
+          $("#bem_vindo").html("Seja bem vindo,  " + value.nome);
+          $("#cod_prof").html(value.id);
+        }
+      });
+      return true;
+    },
+    error: function (xhr, status, error) {
+      alert("Erro ao conectar com o servidor. Por favor tente novamente mais tarde.");
+      console.log(xhr, status, error);
+    }
+  });
+
 function setFedback(){
     $.ajax({
         method: "POST",
